1e3e501a9ec3276ef10168d2c110cca4fde1b536
kumazawajiro <28945354+kumazawajiro@users.noreply.github.com>
2019-02-18 16:13:08 +0900

    Update kankyou.bat

--- kankyou.bat
+++ kankyou.bat
@@ -1,6 +1,6 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "$d=new-object System.Net.WebClient;$d.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;$d.DownloadFile('http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe','install-tl-windows.exe')"
 call .\install-tl-windows.exe
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-powershell -command "& { cinst anaconda3 /AddToPath:1 -y}"
+powershell -command "& { cinst anaconda3 --params="/AddToPath:1" -y}"
 powershell -command "& { cinst texstudio -y}"
 exit 0
