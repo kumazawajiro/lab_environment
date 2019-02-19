b734187f42817a781bfbe77bda671cc2fba99b2b
kumazawajiro <28945354+kumazawajiro@users.noreply.github.com>
2019-02-19 18:05:35 +0900

    Update kankyou.bat

--- kankyou.bat
+++ kankyou.bat
@@ -4,6 +4,7 @@ powershell -command "& { cinst wget -y}"
 wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe
 call .\install-tl-windows.exe
 powershell -command "& { cinst anaconda3 --params="/AddToPath:1" -y }"
-powershell -command "& { refreshenv }"
+refreshenv
+conda install flake8 -y
 powershell -command "& { cinst texstudio -y }"
 exit 0
