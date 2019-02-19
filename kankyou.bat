ab95233fd15fb12189b4e826c58f64490a156e39
kumazawajiro <28945354+kumazawajiro@users.noreply.github.com>
2019-02-19 17:52:58 +0900

    Update kankyou.bat

--- kankyou.bat
+++ kankyou.bat
@@ -1,7 +1,9 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 :@powershell -NoProfile -ExecutionPolicy Bypass -Command "$d=new-object System.Net.WebClient;$d.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;$d.DownloadFile('http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe','install-tl-windows.exe')"
-powershell -command "& { cinst wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe -y}"
+powershell -command "& { cinst wget -y}"
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe
 call .\install-tl-windows.exe
-powershell -command "& { cinst anaconda3 --params="/AddToPath:1" -y}"
-powershell -command "& { cinst texstudio -y}"
+powershell -command "& { cinst anaconda3 --params="/AddToPath:1" -y }"
+powershell -command "& { refreshenv }"
+powershell -command "& { cinst texstudio -y }"
 exit 0
