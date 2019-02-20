d3819a42a040f56da016c881a416550b011d96f9
kumazawajiro <28945354+kumazawajiro@users.noreply.github.com>
2019-02-20 12:01:28 +0900

    Update kankyou.bat

--- kankyou.bat
+++ kankyou.bat
@@ -1,10 +1,8 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-:@powershell -NoProfile -ExecutionPolicy Bypass -Command "$d=new-object System.Net.WebClient;$d.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;$d.DownloadFile('http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe','install-tl-windows.exe')"
 powershell -command "& { cinst wget -y}"
 wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-windows.exe
 call .\install-tl-windows.exe
-powershell -command "& { cinst anaconda3 --params="/AddToPath:1" -y }"
-refreshenv
+powershell -command "& { cinst texstudio anaconda3 --params="/AddToPath:1" -y }"
+call refreshenv
 conda install flake8 -y
-powershell -command "& { cinst texstudio -y }"
 exit 0
